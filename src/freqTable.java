22b1a2f1e098e124aec241ea9a98307cff16a16d
Alex <alex.ts.99@mail.ru>
2020-10-18 22:01:06 +0300

    ver 0.2.1
    Буквы заносятся в таблицу, но частота букв не считается корректно

--- src/freqTable.java
+++ src/freqTable.java
@@ -1,9 +1,9 @@
 import java.util.HashMap;
 
 public class freqTable {
-    private HashMap<Character, Integer> freqTable = new HashMap<>();
+    private HashMap<Character, Double> freqTable = new HashMap<>();
 
-    public freqTable(String text){
+    public freqTable(String text) {
         //init();
         createFreqTable(text);
     }
@@ -18,8 +18,22 @@ public class freqTable {
 
     private void createFreqTable(String text) {
         int counter;
-        char tempChar;
+        int alphabetIndex;
+        char charVar;
+        char[] alphabet = new char[64];
+        for (int i = 0; i < 64; i++) alphabet[i] = (char) ('А' + i);
 
-        
+        for (alphabetIndex = 1092 - 1040; alphabetIndex < alphabet.length; alphabetIndex++) {
+            counter = 0;
+
+            for (int j = 0; j < 15; j++) {
+                charVar = text.charAt(j);
+                if (charVar >= 1040 && charVar <= 1103) {
+                    if (alphabet[alphabetIndex] == charVar) counter++;
+                }
+            }
+
+            freqTable.put(alphabet[alphabetIndex], (double) (counter / text.length()));
+        }
     }
 }
